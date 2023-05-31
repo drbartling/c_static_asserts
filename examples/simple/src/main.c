f37e3c4088a4a368391562779e1b7f73d149cfd0
Ryan Bartling <rbartling@axon.com>
2023-05-31 12:22:49 -0700

    example: Add tagged union structure in example

--- examples/simple/src/main.c
+++ examples/simple/src/main.c
@@ -16,7 +16,28 @@ typedef enum __attribute__((packed)) {
     fruit,
     vegetable,
 } union_tag_t;
-STATIC_ASSERT_TYPE_SIZE(union_tag_t, 1);
+// STATIC_ASSERT_TYPE_SIZE(union_tag_t, 1);
+
+typedef enum __attribute__((packed)) {
+    apple,
+    pear,
+    banana,
+} tasty_fruit_t;
+// STATIC_ASSERT_TYPE_SIZE(tasty_fruit_t, 1);
+
+typedef struct __attribute__((packed)) {
+    int8_t empty[0]; // Empty array for zero size. Vegetables are not tasty /s
+} tasty_vegetable_t;
+// STATIC_ASSERT_TYPE_SIZE(tasty_vegetable_t, 0);
+
+typedef struct __attribute__((packed)) {
+    union_tag_t tag;
+    union {
+        tasty_fruit_t     fruit;
+        tasty_vegetable_t vegetable;
+    };
+} tasty_food_t;
+// STATIC_ASSERT_TYPE_SIZE(tasty_food_t, 2);
 
 int
 main(int argc, char const *argv[])
@@ -25,6 +46,11 @@ main(int argc, char const *argv[])
     printf("sizeof(my_struct_t): %d\r\n", (int)sizeof(my_struct_t));
     printf("Offset of big_num is: %d\r\n", (int)offsetof(my_struct_t, big_num));
     printf("Offset of med_num is: %d\r\n", (int)offsetof(my_struct_t, med_num));
+
+    printf("sizeof(union_tag_t): %d\r\n", (int)sizeof(union_tag_t));
+    printf("sizeof(tasty_fruit_t): %d\r\n", (int)sizeof(tasty_fruit_t));
+    printf("sizeof(tasty_vegetable_t): %d\r\n", (int)sizeof(tasty_vegetable_t));
+    printf("sizeof(tasty_food_t): %d\r\n", (int)sizeof(tasty_food_t));
 
     return 0;
 }
